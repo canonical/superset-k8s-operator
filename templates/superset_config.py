34d97e57c39606929465104cd6c0be9313b693a2
Amber <amber.charitos@canonical.com>
2023-08-10 14:22:47 +0100

    access env variables only when needed

--- templates/superset_config.py
+++ templates/superset_config.py
@@ -1,36 +1,35 @@
 import os
 from cachelib.redis import RedisCache
 
-redis_host = os.getenv("REDIS_HOST")
-redis_port = os.getenv("REDIS_PORT")
-
 # Redis caching
 CACHE_CONFIG = {
     "CACHE_TYPE": "redis",
-    "CACHE_REDIS_URL": f"redis://{redis_host}:{redis_port}/0",
+    "CACHE_REDIS_URL": f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/0",
 }
 # TALISMAN_ENABLED=True
 FILTER_STATE_CACHE_CONFIG = {
     "CACHE_TYPE": "RedisCache",
     "CACHE_DEFAULT_TIMEOUT": 86400,
     "CACHE_KEY_PREFIX": "superset_filter_cache",
-    "CACHE_REDIS_URL": f"redis://{redis_host}:{redis_port}/1",
+    "CACHE_REDIS_URL": f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/1",
 }
 EXPLORE_FORM_DATA_CACHE_CONFIG = {
     "CACHE_TYPE": "RedisCache",
     "CACHE_DEFAULT_TIMEOUT": 86400,
     "CACHE_KEY_PREFIX": "superset_filter_cache",
-    "CACHE_REDIS_URL": f"redis://{redis_host}:{redis_port}/2",
+    "CACHE_REDIS_URL": f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/2",
 }
 DATA_CACHE_CONFIG = {
     "CACHE_TYPE": "SupersetMetastoreCache",
     "CACHE_KEY_PREFIX": "superset_results",
     "CACHE_DEFAULT_TIMEOUT": 86400,
-    "CACHE_REDIS_URL": f"redis://{redis_host}:{redis_port}/3",
+    "CACHE_REDIS_URL": f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/3",
 }
 
 RESULTS_BACKEND = RedisCache(
-    host=redis_host, port=redis_port, key_prefix="superset_results"
+    host=os.getenv("REDIS_HOST"),
+    port=os.getenv("REDIS_PORT"),
+    key_prefix="superset_results",
 )
 
 
