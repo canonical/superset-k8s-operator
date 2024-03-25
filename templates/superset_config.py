760a054e85ba6abeb1cd245156a1e9d09e49985e
Amber Charitos <amber.charitos@canonical.com>
2024-03-25 09:39:31 +0000

    add talisman config + update libs

--- templates/superset_config.py
+++ templates/superset_config.py
@@ -65,7 +65,13 @@ RESULTS_BACKEND = RedisCache(
 TALISMAN_ENABLED = True
 TALISMAN_CONFIG = {
      "force_https": False,
-     "content_security_policy": {}
+     "content_security_policy": {
+        "default-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        "img-src": ["'self'", "data:"],
+        "worker-src": ["'self'", "blob:"],
+        "connect-src": ["'self'", "https://api.mapbox.com", "https://events.mapbox.com"],
+        "object-src": "'none'",
+     }
 }
 
 SQLALCHEMY_POOL_SIZE = int(os.getenv("SQLALCHEMY_POOL_SIZE"))
