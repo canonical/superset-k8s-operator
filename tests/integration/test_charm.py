5610313149a38c94f2a651236bd91bd7e40fdee1
Ali Kelkawi <ali.kelkawi@canonical.com>
2025-05-01 14:52:22 +0300

    remove redundant code blocks

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -5,10 +5,8 @@
 """Charm integration tests."""
 
 import logging
-from pathlib import Path
 
 import pytest
-import pytest_asyncio
 import requests
 from integration.conftest import deploy  # noqa: F401, pylint: disable=W0611
 from integration.helpers import (
@@ -20,33 +18,9 @@ from integration.helpers import (
     restart_application,
     simulate_crash,
 )
-from pytest import FixtureRequest
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
-
-
-@pytest.fixture(scope="module", name="charm_image")
-def charm_image_fixture(request: FixtureRequest) -> str:
-    """The OCI image for charm."""
-    charm_image = request.config.getoption("--superset-image")
-    assert (
-        charm_image
-    ), "--superset-image argument is required which should contain the name of the OCI image."
-    return charm_image
-
-
-@pytest_asyncio.fixture(scope="module", name="charm")
-async def charm_fixture(
-    request: FixtureRequest, ops_test: OpsTest
-) -> str | Path:
-    """Fetch the path to charm."""
-    charms = request.config.getoption("--charm-file")
-    if not charms:
-        charm = await ops_test.build_charm(".")
-        assert charm, "Charm not built"
-        return charm
-    return charms[0]
 
 
 @pytest.mark.abort_on_fail
